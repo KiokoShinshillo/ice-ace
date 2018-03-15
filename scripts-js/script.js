ad0227d37702b5d45b3a77e897be209a4224d732
KiokoShinshillo <36131406+KiokoShinshillo@users.noreply.github.com>
2018-03-16 02:03:43 +0300

    1-2-3 + адаптив( мобила/планшет)

--- scripts-js/script.js
+++ scripts-js/script.js
@@ -43,4 +43,42 @@ $('.gallery__miniature').slick({
   //   // instead of a settings object
   // ]
 });
+
+$('.instructor__conteiner').slick({
+  arrows: false,
+  variableWidth: true,
+  infinite: false,
+  slidesToShow: 1,
+  speed: 800,
+  // draggable: false,
+  touchMove: false,
+  touchThreshold: 100
+
+  // responsive: [
+  //   {
+  //     breakpoint: 1160,
+  //     settings: {
+  //       slidesToShow: 6,
+  //       slidesToScroll: 6
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       slidesToShow: 3,
+  //       slidesToScroll: 1
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       slidesToShow: 1,
+  //       slidesToScroll: 1
+  //     }
+  //   }
+  //   // You can unslick at a given breakpoint now by adding:
+  //   // settings: "unslick"
+  //   // instead of a settings object
+  // ]
+});
 });
